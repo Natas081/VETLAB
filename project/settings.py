e775986b83f0a90dc994399840d66dee9060aafa
Natas081 <nlps@cesar.school>
2025-09-25 09:41:38 -0300

    permitindo o render acessar o site pelo settings

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(3vzc&z%-^+p$$if(2#y&b)txe+a+-pzve0$4ucw-5!&82fcj4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vetlab.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
