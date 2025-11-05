981c52ab277bb0c0fd4fa9b65a6483dbbd9f8314
Natas081 <nlps@cesar.school>
2025-11-05 20:06:11 -0300

    a

--- pets/admin.py
+++ pets/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
-# IMPORTANTE: Importa todos os modelos necessários
+# Importa todos os seus modelos
 from .models import Pet, Evento, Meta, Produto
 
-# Registra cada modelo para que apareça no admin
+# Registra cada um deles
 admin.site.register(Pet)
 admin.site.register(Evento)
-<<<<<<< HEAD
 admin.site.register(Meta)
 admin.site.register(Produto)
-=======
-
->>>>>>> ab1c3985c8395885d62d1fcc56c57ef5fdd44999
